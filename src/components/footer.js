3a4b1406d8e2f5117531e58c71d1ff0097662840
putuduta <putuduta@gmail.com>
2020-06-07 15:51:54 +0700

    Initial Commit

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+import React from "react";
+
+const year = new Date().getFullYear();
+
+function footer() {
+    return (
+        <footer>
+            <p > Copyright © {year} </p>
+        </footer>
+    );
+ }
+
+ export default footer;
